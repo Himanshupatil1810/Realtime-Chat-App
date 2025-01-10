2bc83d289ea48179d5875d775f677ce225b2276f
Himanshu Patil <hp96770@gmail.com>
2025-01-10 20:10:24 +0530

    Added vercel.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,11 +17,12 @@ app.use(express.json({ limit: '10mb' })); // Increase limit to 10MB
 // Middleware to parse URL-encoded data (if needed)
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // For URL-encoded payloads
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-})
-);
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+// })
+// );
+app.use(cors());
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
